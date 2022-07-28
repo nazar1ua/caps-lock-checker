64bd2ac082bdd2ffa68a28fbc3a73e807008a5d8
Nazar Niphone <74293442+nazar1ua@users.noreply.github.com>
2022-07-28 16:51:36 +0300

    init

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-//
+let status = false
+
+document.addEventListener('DOMContentLoaded', e => {
+    if (e.getModifierState('CapsLock')) {
+        console.log('test');
+    }
+    const out = document.getElementById('out')
+})
